439a905e3a87d2dc27e5e580b701752962cec895
GabrielaGonzaga <73176744+GabrielaGonzaga@users.noreply.github.com>
2023-08-08 18:25:19 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Curric0
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-vcz5sp-qwkc4v)
---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'ap1',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  name = 'Crishna ' + VERSION.full;
+}
