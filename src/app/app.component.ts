c322af18ffb22fe67f3200276b5871bfc240b6bd
GabrielaGonzaga <73176744+GabrielaGonzaga@users.noreply.github.com>
2023-08-08 21:00:44 -0300

    6 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component, VERSION } from '@angular/core';
 
 @Component({
-  selector: 'ap1',
+  selector: 'ProfileComponent',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  name = 'Crishna ' + VERSION.full;
+  name = 'Gabriela Gonzaga';
 }
